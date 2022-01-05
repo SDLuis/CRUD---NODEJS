d4782a9932838463af2f120df43cf7c2f0d38c2b
Luis <Luistejeda0011@gmail.com>
2022-01-04 23:37:56 -0400

    Adding database status filter

--- Database/database.js
+++ Database/database.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const { promisify } = require("util");
 
 const connection = mysql.createConnection({
   host: "biwykrp7dwrapbsl3xzb-mysql.services.clever-cloud.com",
@@ -7,5 +8,26 @@ const connection = mysql.createConnection({
   password: "sqJdKk0ewq0ilK7C5o8q",
   database: "biwykrp7dwrapbsl3xzb",
 });
+
+const pool = mysql.createPool(connection);
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error / "DATABASE CONNECTION WAS CLOSED";
+    }
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error / "DATABASE HAS TOO MANY CONNECTIONS";
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error / "DATABASE CONNECTION WAS REFUSED";
+    }
+  }
+
+  if (connection) connection.release();
+  console.log("Base de datos conectada");
+});
+
+pool.query = promisify(pool.query);
 
 module.exports = connection;
