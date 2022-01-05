9d2fe6d2bdc1a475db5ac4d1a61aab23570abd82
Luis <Luistejeda0011@gmail.com>
2022-01-04 23:42:47 -0400

    Adding routes and some modules

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
-
 const app = express()
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('port', process.env.PORT || 5000)
+app.use(require("./Routes/opinions"))
+app.use(require("./Routes/users"))
+app.use(require("./Routes/register"))
+
 
 app.get('/' , (req , res) =>{
     res.send('Done')
